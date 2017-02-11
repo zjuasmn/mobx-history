0bb50af70fd8e9d4527501b641cbc0797a7cebec
asmn <a_smn@qq.com>
2017-02-11 10:45:50 +0800

    fix export

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import History from "./History"
-exports.History = History;
-export default History
+export default from "./History"
+export History from './History'
 export createBrowserHistory from './createBrowserHistory'
 export createHashHistory from './createHashHistory'
 export createMemoryHistory from './createMemoryHistory'
