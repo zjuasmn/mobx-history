4134fa6cc2e8a7bf601494819ac07bfbdeefbccb
Dmitry Jakubovski <dmitry.jakubovsky@gmail.com>
2018-04-09 18:35:12 +0300

    update to mobx 4.x.x

--- src/History.js
+++ src/History.js
@@ -1,4 +1,4 @@
-import {action, Atom, extendObservable, observable} from "mobx";
+import {action, createAtom, extendObservable, observable} from "mobx";
 const debug = require('debug')('mobx-history');
 const reservedKeys = {location: true, action: true, length: true};
 
@@ -8,26 +8,26 @@ export default class History {
   @observable _location = {};
   @observable _action;
   @observable _length;
-  
+
   set location(location) {
     this.history.push(location);
   }
-  
+
   get location() {
     this.atom.reportObserved();
     return this._location;
   }
-  
+
   get action() {
     this.atom.reportObserved();
     return this._action;
   }
-  
+
   get length() {
     this.atom.reportObserved();
     return this._length;
   }
-  
+
   constructor(history) {
     this.history = history;
     for (let key in history) {
@@ -46,9 +46,9 @@ export default class History {
       this._action = this.history.action;
       this._length = this.history.length;
     });
-    
+
     let handler = null;
-    this.atom = new Atom('History',
+    this.atom = createAtom('History',
       () => {
         debug('begin listen');
         update();
