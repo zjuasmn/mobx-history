18b24e849b627ca0c810f027e933e59508f042ee
Dmitry Jakubovsky <dmitry.jakubovsky@gmail.com>
2018-04-18 08:26:21 +0300

    update to mobx 4.x.x (#1)
    * update to mobx 4.x.x
    
    * fix tests

--- src/History.js
+++ src/History.js
@@ -1,4 +1,4 @@
-import {action, Atom, extendObservable, observable} from "mobx";
+import {action, createAtom, extendObservable, observable} from "mobx";
 const debug = require('debug')('mobx-history');
 const reservedKeys = {location: true, action: true, length: true};
 
@@ -8,26 +8,26 @@ export default class History {
   @observable _location = {};
   @observable _action;
   @observable _length;
-  
+
   set location(location) {
     this.history.push(location);
   }
-  
+
   get location() {
     this.atom.reportObserved();
     return this._location;
   }
-  
+
   get action() {
     this.atom.reportObserved();
     return this._action;
   }
-  
+
   get length() {
     this.atom.reportObserved();
     return this._length;
   }
-  
+
   constructor(history) {
     this.history = history;
     for (let key in history) {
@@ -40,15 +40,24 @@ export default class History {
         })
       })(key);
     }
+
     const update = action((location, action) => {
       debug(this.location, location, action);
-      extendObservable(this._location, this.history.location);
+
+      for (let key in this.history.location) {
+        if (this._location.hasOwnProperty(key)) {
+          this._location[key] = this.history.location[key];
+         } else {
+          extendObservable(this._location, { [key]: this.history.location[key] });
+        }
+      }
+
       this._action = this.history.action;
       this._length = this.history.length;
     });
-    
+
     let handler = null;
-    this.atom = new Atom('History',
+    this.atom = createAtom('History',
       () => {
         debug('begin listen');
         update();
